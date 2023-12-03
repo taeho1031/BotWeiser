2ce7f39cbdce03faa0d00f18ea148ad718a29449
KenjiKano1 <91629339+KenjiKano1@users.noreply.github.com>
2023-12-03 02:09:42 -0800

    Add files via upload

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,25 +4,25 @@ import "./Footer.css";
 const Footer=()=>{
     return(
         <div className="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <a href="https://forta.org/" className="link" >
+            <div className="footer-container">
+                <div className="footer-row">
+                    <div className="footer-col">
+                        <a href="https://forta.org/" className="footer-link" >
                             &copy;{new Date().getFullYear()} Forta Foundation
                         </a>
                         
                     </div>
 
-                    <div className="col-left">
-                        <a href="https://discord.gg/KACdTEutQq" className="link">Discord</a>
+                    <div className="footer-col">
+                        <a href="https://discord.gg/KACdTEutQq" className="footer-link">Discord</a>
                     </div>
 
-                    <div className="col-left">
-                        <a href="https://forta.org/legal/" className="link">Term of Service</a>
+                    <div className="footer-col">
+                        <a href="https://forta.org/legal/" className="footer-link">Term of Service</a>
                     </div>
 
-                    <div className="col-left">
-                        <a href="https://forta.org/privacy/" className="link">Privacy Policy</a>
+                    <div className="footer-col">
+                        <a href="https://forta.org/privacy/" className="footer-link">Privacy Policy</a>
                     </div>
 
                 </div>
