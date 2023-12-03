f329990512cc91b070ac288c85fb8f235c745ac0
junhwanlee234 <junhwanlee2316@gmail.com>
2023-12-02 22:42:54 -0800

    Bot Card Style

--- app/src/components/search/response/cards/BotCard.js
+++ app/src/components/search/response/cards/BotCard.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "./BotCard.css";
 
 export const BotCard = ({ id, name, description }) => {
   return (
     <div className="wrapper">
-      <div className="BotCard__id">{id}</div>
+      <div className="BotCard__id">
+        <text>Bot ID:</text>
+        {id}
+      </div>
       <div className="BotCard__heading">
         <div className="BotCard__title">{name}</div>
       </div>
