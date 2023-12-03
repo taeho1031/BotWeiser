23d183b68e91de753b7085cf34c07cb473b71edc
Taeho Kim <taehokim@ucsb.edu>
2023-12-03 00:50:12 -0800

    Implemented BotCard

--- app/src/components/search/response/cards/BotCard.js
+++ app/src/components/search/response/cards/BotCard.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./BotCard.css";
 
 export const BotCard = ({ id, name, description }) => {
+  const botUrl = `https://app.forta.network/bot/${id}`;
+
   return (
     <div className="wrapper">
-      <div className="BotCard__id">
-        <text>Bot ID:</text>
-        {id}
-      </div>
-      <div className="BotCard__heading">
-        <div className="BotCard__title">{name}</div>
-      </div>
+      <a className="BotCard__title" href={botUrl} target="_blank" rel="noopener noreferrer">
+        {name}
+      </a>
       <div className="BotCard__description">{description}</div>
     </div>
   );
