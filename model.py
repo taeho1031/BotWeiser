d6f7edadf7a0d62eff0d729ce76da7eb25f964c8
Abelatnafu <Abeljayirv@gmail.com>
2024-02-16 18:18:57 -0800

    Starter code for Json formatter GPT API

---
+++ model.py
@@ -0,0 +1,47 @@
+import openai
+import os
+import json
+
+
+
+class JsonGPT:
+    def __init__(self, query):
+        self.model = "gpt-4"
+        self.prompt = "create me a json object with the following attributes:\
+            {\
+                id:\
+                description:\
+                timestamp:\
+                chainID:\
+            }\
+            if timestamp is not specified use current time.\
+                "
+        self.query = query
+        
+
+        
+    def parse_input(self):
+        prompt = f"{self.prompt}  \n Query:```{self.query}```"
+        return prompt
+
+    def api_request(self):
+        from dotenv import load_dotenv, find_dotenv
+        _ = load_dotenv(find_dotenv())
+        openai.api_key  = os.getenv('OPENAI_API_KEY')
+        messages = [{"role": "user", "content": self.parse_input()}]
+        client = openai.OpenAI()
+        response = client.chat.completions.create(
+        model=self.model,
+        messages=messages)
+        return response.choices[0].message
+    
+    def getJsonObjectSearch(self):
+        gpt_response = self.api_request()
+        return json.loads(gpt_response.content)
+
+
+        
+search = "I want a bot that can detect money lanundering in the etherum blockchain network."
+norf = JsonGPT(search)
+j = norf.getJsonObjectSearch()
+print(f"{j=}")
