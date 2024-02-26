ea234dc7f7fe7db982a96fdb5c56f87d197453eb
Abelatnafu <Abeljayirv@gmail.com>
2024-02-26 00:55:19 -0800

    reformat gpt promp

--- GPT/GPT Connect/GPTConnect.py
+++ GPT/GPT Connect/GPTConnect.py
@@ -2,27 +2,12 @@ import openai
 import os
 import json
 from datetime import datetime, timezone
-# from prompts.jsonPrompts import json_prompt
+from prompts.jsonPrompts import json_prompt
 
 class JsonGPT:
     def __init__(self, query):
         self.model = "gpt-4"
-        self.jsonPrompt = "convert the query supplied at the end inside triple backticks to a single json object\
-            Additional information:\
-            Chain Ids(only include these chain Ids): \
-            - 1: Ethereum Mainnet \
-            - 42114: Avalanche Fuji C-Chain \
-            - 5: Goerli (Ethereum Testnet) \
-            - 137: Polygon (formerly Matic) \
-            - 10: Optimistic Ethereum (Optimism) \
-            - 43114: Avalanche X-Chain \
-            - 42220: Celo Mainnet \
-            - 42161: Arbitrum \
-            - 56: Binance Smart Chain \
-            - 250: Fantom Opera \
-            If timestamp is not specified use current time which is " + " {}.\
-            If a blockchain network is provided make sure to add all correct chainIDs relevant.\
-            ".format(datetime.now(timezone.utc))
+        self.jsonPrompt = json_prompt
         self.query = query
         self.bots = []
         self.conversation = [query]
