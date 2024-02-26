a1fe76570be3f75590c35139906bf68135019ccb
Abelatnafu <Abeljayirv@gmail.com>
2024-02-26 00:53:33 -0800

    delete none GPT files

--- app/src/components/search/response/UserResponse.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ResponseCard } from "./cards/ResponseCard";
-import "./UserResponse.css";
-
-export const UserResponse = ({ userInput }) => {
-  return <ResponseCard isBot={false}>{userInput}</ResponseCard>;
-};
